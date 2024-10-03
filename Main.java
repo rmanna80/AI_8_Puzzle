eb3b7200771c2f179c2aa4bb61a1130edae45220
farmerismyname <benjaminlfarmer@icloud.com>
2024-10-03 18:10:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,36 @@ class Main{
     public static void main(String[] args){
 
         // CALL THE FUNCTIONS TO RUN PROGRAM 
+        run();
+    }
+
+    public void run(){
 
     }
+
+    public moveLeft(){
+    }
+     public moveRight(){
+    }
+     public moveDown(){
+    }
+     public moveUp(){
+    }
+    public testAll(){
+        rightError = moveRight(currentState);
+    string userInput = input("enter a 9 digit number using digits 0-8 only once");
+        if len(user_input) != 9
+    
+    array[] currentState = userInput
+    array goalState1 = [1,1];
+    array goalState2 = [1,2];
+    array goalState3 = [1,3];
+    array goalState4 = [2,1];
+    array goalState5 = [2,2];
+    array goalState6 = [2,3];
+    array goalState7 = [3,1];
+    array goalState8 = [3,2];
+    array goalState9 = [3,3];
+    
 
 }
